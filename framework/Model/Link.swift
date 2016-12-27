aa225186e37ab1b6608cd1f91b265e77c98ecf75
ccrama <hapticappsdev@gmail.com>
2016-12-27 11:07:22 -0600

    Fixed small issue

--- framework/Model/Link.swift
+++ framework/Model/Link.swift
@@ -284,6 +284,7 @@ public struct Link: Thing, Created, Votable {
         visited = false
         numReports = 0
         distinguished = .none
+        baseJson = json
         media = Media(json: [:])
         mediaEmbed = MediaEmbed(json: [:])
         
