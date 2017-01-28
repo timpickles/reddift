39abb3a1b27387effaeb8fdd9232bd4ad1f8cdbd
Carlos Crane <hapticappsdev@gmail.com>
2017-01-28 01:19:13 -0600

    Update Session+search.swift

--- framework/Network/Session+search.swift
+++ framework/Network/Session+search.swift
@@ -29,6 +29,9 @@ extension Session {
             path = subreddit.path + "/search"
             restrict = true
         }
+        if(subreddit != nil && subreddit!.displayName == "all"){
+            restrict = false
+        }
         
         let parameter = paginator.dictionaryByAdding(parameters: ["q":query, "sort":sort.path, "restrict_sr": restrict ? "yes" : "no"])
         
