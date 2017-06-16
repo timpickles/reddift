eaaac22ced76a36fb61b812b6f7768ee90f3fd0d
Carlos Crane <hapticappsdev@gmail.com>
2017-06-16 10:40:54 -0500

    Update Link.swift

--- reddift/Model/Link.swift
+++ reddift/Model/Link.swift
@@ -316,7 +316,10 @@ public struct Link: Thing {
         } else {
             likes = .none
         }
-        linkFlairText = data["link_flair_text"] as? String ?? ""
+        
+        let tempFlair = data["link_flair_text"] as? String ?? ""
+        linkFlairText = tempFlair.gtm_stringByUnescapingFromHTML()
+        
         gilded = data["gilded"] as? Int ?? 0
         archived = data["archived"] as? Bool ?? false
         locked = data["locked"] as? Bool ?? false
