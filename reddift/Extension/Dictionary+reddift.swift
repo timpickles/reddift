359d04ccbefdeed32bf1a753e1e2d31423654dc8
Carlos Crane <hapticappsdev@gmail.com>
2018-08-29 15:20:23 -0500

    Update Dictionary+reddift.swift

--- reddift/Extension/Dictionary+reddift.swift
+++ reddift/Extension/Dictionary+reddift.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-private let allowedCharacterSet = CharacterSet(charactersIn: "!$&'()*+,-./0123456789:=?@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz~")
+private let allowedCharacterSet = CharacterSet(charactersIn: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~-_.!*'(),")
 
 /**
 Protocol to generate URL query string from Dictionary[String:String].
